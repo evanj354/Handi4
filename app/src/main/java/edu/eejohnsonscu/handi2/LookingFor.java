ecaddcd90fc24cb6deca5cc59145b446deebbba2
evanj354 <eejohnson@scu.edu>
2017-03-04 17:32:07 -0800

    Initial commit

---
+++ app/src/main/java/edu/eejohnsonscu/handi2/LookingFor.java
@@ -0,0 +1,16 @@
+package edu.eejohnsonscu.handi2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LookingFor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_looking_for);
+    }
+
+
+}
---
+++ app/src/main/java/edu/eejohnsonscu/handi2/Work.java
@@ -0,0 +1,13 @@
+package edu.eejohnsonscu.handi2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Work extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work);
+    }
+}
